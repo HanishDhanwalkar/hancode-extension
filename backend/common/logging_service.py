0a948c8e523f7105e8e98e585554979220534596
HanishDhanwalkar <hanishpd1729@gmail.com>
2026-05-17 23:22:47 +0530

    logging service

--- backend/common/logging_service.py
+++ backend/common/logging_service.py
@@ -1,12 +1,22 @@
 import logging
+import sys
 
 
 class Logger:
     def __init__(self, name, set_level=logging.INFO):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(set_level)
+
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(set_level)
+
         self.formatter = logging.Formatter(
-            '%(asctime)s - %(levelname)s - %(message)s')
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        console_handler.setFormatter(self.formatter)
+
+        if not self.logger.handlers:
+            self.logger.addHandler(console_handler)
 
     def get_logger(self):
         return self.logger
